0dd34c6791f9a4dd59f4452a596b585ec2eea1ee
Kim Jinhyeok <derrick11@naver.com>
2020-09-02 16:32:07 +0900

    이미지 저장기능 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -13,8 +14,12 @@ canvas.height = CANVAS_SIZE;
 
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+//캔버스 기본 배경색 지정
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE)
 
 ctx.fillStyle = INITIAL_COLOR;
+
 
 let painting = false;
 let filling = false;
@@ -72,6 +77,18 @@ function handleCanvasClick() {
     }
 }
 
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
+
 // 이벤트 함수 선언
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -79,6 +96,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -89,4 +107,8 @@ if (range) {
 
 if (mode) {
     mode.addEventListener("click",handleModeClick)
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener("click",handleSaveClick)
 }
