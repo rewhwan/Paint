f8331c48a26e2cd8bb5a78d807a4a88a328e828e
Kim Jinhyeok <derrick11@naver.com>
2020-09-02 14:15:01 +0900

    -브러시 사이즈 변경기능 추가 및 주석 추가

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// html객체 상수 선언
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -18,28 +20,41 @@ function startPainting() {
     painting = true;
 }
 
+// 캔버스 이벤트 함수
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting) {
+    if (!painting) {
         ctx.beginPath()
-        ctx.moveTo(x,y);
+        ctx.moveTo(x, y);
     } else {
-        ctx.lineTo(x,y);
+        ctx.lineTo(x, y);
         ctx.stroke();
     }
 }
 
+// 브러시 색상을 바꾸는 함수
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
 }
 
-if(canvas) {
-    canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",startPainting);
-    canvas.addEventListener("mouseup",stopPainting)
-    canvas.addEventListener("mouseleave",stopPainting)
+// 브러시 사이즈 바꾸는 함수
+function handleRangeChange(event) {
+    const  size = event.target.value;
+    ctx.lineWidth = size;
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick));
+// 이벤트 함수 선언
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting)
+    canvas.addEventListener("mouseleave", stopPainting)
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if (range) {
+    range.addEventListener("input", handleRangeChange)
+}
