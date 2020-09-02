7c289bc7dc44a9a75dc45cc25a56223f294b96cf
Kim Jinhyeok <derrick11@naver.com>
2020-09-02 15:27:28 +0900

    -채우기 모드 기능 추가

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
+ctx.fillStyle = INITIAL_COLOR;
+
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -37,6 +44,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 // 브러시 사이즈 바꾸는 함수
@@ -45,16 +53,40 @@ function handleRangeChange(event) {
     ctx.lineWidth = size;
 }
 
+// 버튼 클릭 이벤트 함수
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    }else {
+        filling = true;
+        mode.innerText = "Paint"
+
+    }
+}
+
+// 채우기 이벤트 함수
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE)
+    }
+}
+
 // 이벤트 함수 선언
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("click",handleCanvasClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 
 if (range) {
     range.addEventListener("input", handleRangeChange)
+}
+
+if (mode) {
+    mode.addEventListener("click",handleModeClick)
 }
